336e363da781471469d04efe6fba5194fb2a1902
lebrancconvas <lebrancconvas@gmail.com>
2022-01-15 03:41:40 +0700

    🔨 Edit Setter Getter in C++ .

--- settergetter.cpp
+++ settergetter.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
 
-class Shape
+class Rectangle
 {
-	public:
-		void setHeight(int h) {height = h;}
-		void setWidth(int w) {width = w;}
-	protected:
-		int height;
+	public:	
+		void setWidth(int w) { width = w; }
+		void setHeight(int h) { height = h; }
+		int getWidth() { return width; }
+		int getHeight() {return height; }
+		int getArea() { return width * height; }
+	private:
 		int width;
-};
-
-class Rectangle : public Shape
-{
-	public:
-		int area;
-		int getArea()
-		{
-			area = height * width;
-			return area;
-		}
+		int height;
 };
 
 int main()
 {
-	Rectangle buckshape;
-	buckshape.setHeight(20);
-	buckshape.setWidth(70);
-	std::cout << "Area of the shape: " << buckshape.getArea() << std::endl; 
+	Rectangle rectshape;
+	rectshape.setWidth(20);
+	rectshape.setHeight(70);
+	std::cout << "Rectangle Information" << std::endl
+		<< "Rectangle Width: " << rectshape.getWidth() << std::endl
+		<< "Rectangle Height: " << rectshape.getHeight() << std::endl
+		<< "Rectangle Area: " << rectshape.getArea() << std::endl; 
 	return 0;
 }

d7bfaedfbb61970d1088d3f86f2dfeec45ef429f
lebrancconvas <lebrancconvas@gmail.com>
2022-01-15 02:03:34 +0700

    💡 Add Comments to explain the algorithm of this function.

--- function.cpp
+++ function.cpp
@@ -13,11 +13,14 @@ int minus(int number1, int number2)
 
 int main()
 {
+	// Init the variables. 
 	int a = 1;
 	int b = 2;
 	int result = 0;
 	std::string symbol = "";
 	std::string operation = "ADD";
+
+	// Set the condition of calculation. 
 	if(operation == "ADD")
 	{
 		symbol = "+";
@@ -33,5 +36,7 @@ int main()
 		symbol = "xxx";
 		result = 0;
 	}
+
+	//display the result. 
 	std::cout << "Result Of " << a << " " << symbol << " " << b << " is " << result << std::endl;
 } 

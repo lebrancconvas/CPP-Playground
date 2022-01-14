e4c6f52bb2cee8b9e988dc892e3840958ea79807
lebrancconvas <lebrancconvas@gmail.com>
2022-01-15 02:22:40 +0700

    💡 Add Comment to explain the concept of C++'s address and pointer.

--- pointer.cpp
+++ pointer.cpp
@@ -2,8 +2,13 @@
 
 int main()
 {
+	// a is the variable's label, and 5 is the variable's value. 
 	int a = 5;
+
+	// &a is the address of variable "a". and * is the symbol for access the address value. 
 	int* adda = &a;
+
+	// *adda is how to access the value of variable that is in the address "adda". 
 	int addavalue = *adda;
 
 	std::cout << addavalue << std::endl; 

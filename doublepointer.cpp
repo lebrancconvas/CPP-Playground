6c6fda21d0e7c23c8a69d9dc4d7c6d3eda5cb461
lebrancconvas <lebrancconvas@gmail.com>
2022-01-15 02:38:51 +0700

    🎨 Beautify the format of cout.

--- doublepointer.cpp
+++ doublepointer.cpp
@@ -7,6 +7,10 @@ int main()
 	int **addressofaddressofa = &addressofa;
 	int valueofaddressofa = *addressofa;
 	int valueofaddressofaddressofa = **addressofaddressofa;
-	std::cout << "a: " << a << std::endl << "Address of a: " << addressofa << std::endl << "Address of address of a: " << addressofaddressofa << std::endl << "Value of Address of a: " << valueofaddressofa << std::endl << "Value of Address of Address of a: " << valueofaddressofaddressofa << std::endl;
+	std::cout << "a: " << a << std::endl 
+		<< "Address of a: " << addressofa << std::endl 
+		<< "Address of address of a: " << addressofaddressofa << std::endl 
+		<< "Value of Address of a: " << valueofaddressofa << std::endl 
+		<< "Value of Address of Address of a: " << valueofaddressofaddressofa << std::endl;
 	return 0; 
 }

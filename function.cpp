e546d7a4291321911fa3e349eda983c0d2c852b9
lebrancconvas <lebrancconvas@gmail.com>
2022-01-15 02:00:12 +0700

    ✨ Create Minus Function and Edit function.cpp

--- function.cpp
+++ function.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
+#include <string>
 
 int add(int number1, int number2)
 {
 	return number1 + number2;
 }
 
+int minus(int number1, int number2)
+{
+	return number1 - number2;
+}
+
 int main()
 {
 	int a = 1;
 	int b = 2;
-	std::cout << "Result Of " << a << " + " << b << " is " << add(1,2) << std::endl;
+	int result = 0;
+	std::string symbol = "";
+	std::string operation = "ADD";
+	if(operation == "ADD")
+	{
+		symbol = "+";
+		result = add(a, b);
+	}
+	else if(operation == "MINUS")
+	{
+		symbol = "-";
+		result = minus(a, b);
+	}
+	else
+	{
+		symbol = "xxx";
+		result = 0;
+	}
+	std::cout << "Result Of " << a << " " << symbol << " " << b << " is " << result << std::endl;
 } 

f0e1e627407e2599d60405100b323aefd6484768
lebrancconvas <lebrancconvas@gmail.com>
2022-01-15 13:55:12 +0700

    ✨ Create Const Parameter Reference in C++ .

---
+++ 10_constreference.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Device
+{
+	public:
+		Device(const int& _id) {id = _id;}
+		int getID() const {return id;}
+	private:
+		int id;
+};
+
+class Window
+{
+	public:
+		void setup(const Device& device)
+		{
+			std::cout << "Setup Device: " << device.getID() << std::endl;
+		}
+};
+
+int main()
+{
+	Device samsung(5);
+	Window window;
+	std::cout << "Device ID: " << samsung.getID() << std::endl;
+	window.setup(samsung);
+	return 0;
+}
